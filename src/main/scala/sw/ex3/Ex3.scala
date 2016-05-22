76268e121774be7fc892750bb51e83cd7c0b9813
Pawel Szulc <paul.szulc@gmail.com>
2016-05-22 13:30:57 +0200

    Add Ex3.Starting2 example

--- src/main/scala/sw/ex3/Ex3.scala
+++ src/main/scala/sw/ex3/Ex3.scala
@@ -18,7 +18,28 @@ object Startings extends App {
     .reduceByKey {
       case (acc, length) => acc + length
     }
-    .sortByKey()
+
+  startings.take(5).foreach(println)
+
+  sc.stop()
+}
+
+object Startings2 extends App {
+
+  val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("local[*]")
+  val sc = new SparkContext(sparkConf)
+
+  val allShakespeare = sc.textFile("src/main/resources/all-shakespeare.txt")
+
+  val startings = allShakespeare
+    .filter(_.trim != "")
+    .groupBy(_.charAt(0))
+    .mapValues(_.size)
+    .reduceByKey {
+      case (acc, length) => acc + length
+    }
 
   startings.take(5).foreach(println)
 
