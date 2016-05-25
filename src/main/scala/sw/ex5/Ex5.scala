dd11f14c3c40e5e9730acefadbef9f0edbecfb2c
Pawel Szulc <paul.szulc@gmail.com>
2016-05-25 16:43:20 -0600

    Add Ex5

---
+++ src/main/scala/sw/ex5/Ex5.scala
@@ -0,0 +1,33 @@
+package sw.ex5
+
+import org.apache.spark._
+
+case class User(name: String)
+
+object SerializationIssues extends App {
+  val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("spark://localhost:7077")
+  val sc = new SparkContext(sparkConf)
+ 
+  val five = sc
+    .parallelize(List("john", "mike", "kate", "anna"))
+    .take(5)
+    .foreach(println)
+
+  sc.stop()
+}
+
+class Simple {
+
+  def iTakeLambda(lambda: Int => String): String = {
+    // val lambda: Int => String = a => a.toString
+    lambda(10)
+  }
+
+  iTakeLambda(a => a.toString())
+}
+
+
+
+
