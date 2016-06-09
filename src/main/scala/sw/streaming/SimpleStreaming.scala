e86300bda55797da5ba462bf9a6b052456812cdf
Pawel Szulc <paul.szulc@gmail.com>
2016-06-09 22:17:07 +0100

    Add SimpleStreaming examples

---
+++ src/main/scala/sw/streaming/SimpleStreaming.scala
@@ -0,0 +1,64 @@
+package sw.streaming
+
+import org.apache.spark._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+
+object SimpleStreaming extends App {
+
+  val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("local[*]")
+  val sc = new SparkContext(sparkConf)
+
+  val ssc = new StreamingContext(sc, Seconds(10))
+  val lines = ssc.socketTextStream("localhost", 9999)
+
+  lines.print()
+
+  ssc.start()
+
+  ssc.awaitTermination()
+}
+
+object SimpleStreaming2 extends App {
+
+  val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("local[*]")
+  val sc = new SparkContext(sparkConf)
+
+  val ssc = new StreamingContext(sc, Seconds(10))
+  val lines = ssc.socketTextStream("localhost", 9999)
+
+  lines.count().print()
+
+  ssc.start()
+
+  ssc.awaitTermination()
+}
+
+object SimpleStreaming3 extends App {
+
+  val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("local[*]")
+  val sc = new SparkContext(sparkConf)
+
+  val ssc = new StreamingContext(sc, Seconds(10))
+  val lines = ssc.socketTextStream("localhost", 9999)
+
+  lines.flatMap(_.split("""\W+"""))
+    .map(w => (w, 1))
+    .reduceByKey(_ + _)
+    .print()
+
+  ssc.start()
+
+  ssc.awaitTermination()
+}
+
+
