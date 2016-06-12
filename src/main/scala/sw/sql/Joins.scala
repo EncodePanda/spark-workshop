5e5deec4f28de77597376bfd39b11cb7a1d2073f
Pawel Szulc <paul.szulc@gmail.com>
2016-06-12 13:02:34 +0200

    Add Joins example

---
+++ src/main/scala/sw/sql/Joins.scala
@@ -0,0 +1,28 @@
+package sw.sql
+
+import org.apache.spark._
+import org.apache.spark.sql.SQLContext
+import sw.air._
+
+object JoinQueries extends App {
+
+  val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("local[*]")
+  val sc = new SparkContext(sparkConf)
+  val sqlCtx = new SQLContext(sc)
+  import sqlCtx._
+
+  val airlines = Airline(sqlCtx)
+  airlines.registerTempTable("airlines")
+
+  val routes = Route(sqlCtx)
+  routes.registerTempTable("routes")
+
+  sql("""select a.name, r.sourceAirport, r.destinationAirport 
+         from airlines a join routes r 
+         on a.id = r.airlineId""").show()
+
+  sc.stop()
+
+}
