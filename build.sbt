d01ecdaf6a1f224da12bd962fdcda619f7ba6c31
Pawel Szulc <paul.szulc@gmail.com>
2016-06-15 01:28:56 +0200

    Add Kafka example

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "1.6.1" % "provided",
   "org.apache.spark" %% "spark-hive" % "1.6.1" % "provided",
   "org.apache.spark" %% "spark-streaming" % "1.6.1" % "provided",
+  "org.apache.spark" %% "spark-streaming-kafka" % "1.6.1" % "provided",
   "com.github.melrief" %% "purecsv" % "0.0.6",
   compilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
 )
---
+++ src/main/scala/sw/streaming/Kafka.scala
@@ -0,0 +1,35 @@
+package sw.streaming
+
+import org.apache.spark._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.kafka._
+
+object Kafka extends App {
+
+    val sparkConf = new SparkConf()
+    .setAppName(this.getClass.getName)
+    .setMaster("local[*]")
+  val interval = 10
+  val sc = new SparkContext(sparkConf)
+  val ssc = new StreamingContext(sc, Seconds(interval))
+  ssc.checkpoint("checkpoint")
+
+
+  val topics = Map("test" -> 1)
+  val zkQuorum = "localhost:2181"
+  val group = "1"
+  val topicLines = KafkaUtils.createStream(ssc, zkQuorum, group, topics)
+  topicLines.map(_._2).print()
+
+  ssc.start()
+
+  ssc.awaitTermination()
+  sc.stop()
+
+}
+
+
